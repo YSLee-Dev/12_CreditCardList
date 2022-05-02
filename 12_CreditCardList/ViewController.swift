233c43ff3a4a98a810ce845b43b5961ce4f02d94
YSLee-Dev <ysleedev01@gmail.com>
2022-05-02 19:09:25 +0900

    FireStore 사용

--- 12_CreditCardList/ViewController.swift
+++ 12_CreditCardList/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 import Kingfisher
 import FirebaseDatabase
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
-    var ref : DatabaseReference! // 파이어베이스 레퍼런스(데이터베이스의 루트)
+    // var ref : DatabaseReference! // 파이어베이스 레퍼런스(데이터베이스의 루트)
+    var db = Firestore.firestore()
     
     lazy var mainTableView : UITableView =  {
         let table = UITableView(frame: .zero, style: .insetGrouped)
@@ -45,6 +47,7 @@ class ViewController: UIViewController {
     }
     
     func databaseSet(){
+        /* 파이어베이스 리얼타임
         self.ref = Database.database().reference()
         ref.observe(.value){ snapshot in
             guard let value = snapshot.value as? [String : [String:Any]] else {return}
@@ -62,6 +65,31 @@ class ViewController: UIViewController {
                 }
             }catch{
                 print("Error JSON : \(error)")
+            }
+        }
+         */
+        
+        // 파이어베이스 파이어스토어
+        self.db.collection("creditCardList").addSnapshotListener{ snapshot, error in
+            guard let document = snapshot?.documents else{
+                print("ERROR Document nil")
+                return
+            }
+            
+            self.creditCardList = document.compactMap{ doc -> CreditCard? in
+                do{
+                    let jsonData = try JSONSerialization.data(withJSONObject: doc.data(), options: [])
+                    let creditCard = try JSONDecoder().decode(CreditCard.self, from: jsonData)
+                    return creditCard
+                }catch{
+                    print(error)
+                    return nil
+                }
+            }.sorted{
+                $0.rank < $1.rank
+            }
+            DispatchQueue.main.async {
+                self.mainTableView.reloadData()
             }
         }
     }
@@ -91,6 +119,7 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource{
         detailV.detailData = self.creditCardList[indexPath.row].promotionDetail
         self.navigationController?.pushViewController(detailV, animated: true)
         
+        /*
         // 옵션 1 == 경로를 알 때
         // let cardID = self.creditCardList[indexPath.row].id
         // ref.child("Item\(cardID)/isSelected").setValue(true)
@@ -100,6 +129,7 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource{
             guard let self = self, let value = snapshot.value as? [String : [String:Any]], let key = value.keys.first else {return}
             self.ref.child("\(key)/isSelected").setValue(true)
         }
+         */
     }
     
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -108,11 +138,13 @@ extension ViewController : UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete{
+            /*
             // 경로를 모르는 경우
             ref.queryOrdered(byChild: "id").queryEqual(toValue: self.creditCardList[indexPath.row].id).observe(.value){ [weak self] snapshot in
                 guard let self = self, let value = snapshot.value as? [String:[String:Any]], let key = value.keys.first else {return}
                 self.ref.child(key).removeValue()
             }
+             */
         }
     }
 }
